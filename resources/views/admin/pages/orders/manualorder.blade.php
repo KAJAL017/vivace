8e75aa283d2e591997934890249f950e018a768d
Kajal <733kajaldas@gmail.com>
2025-02-21 08:32:47 +0530

    test

--- resources/views/admin/pages/orders/manualorder.blade.php
+++ resources/views/admin/pages/orders/manualorder.blade.php
@@ -75,7 +75,7 @@
                                                         <button class="btn btn-success btn-sm">Push To Shiprocket</button>
                                                     </a>
                                                     <a href="#" class="" data-id="{{ $order->id }}">
-                                                        <button class="btn btn-success btn-sm">Push To Manual</button>
+                                                        <button class="btn btn-primary btn-sm">Push To Manual</button>
                                                     </a>
                                                     <a href="javascript:void(0)" class="btn btn-soft-danger btn-sm delete-btn mx-2">
                                                         <iconify-icon icon="solar:trash-bin-minimalistic-2-broken" class="align-middle fs-18"></iconify-icon>
