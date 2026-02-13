41bfdba80c3869a439ec34b0d8077ccf56283c29
Kajal Das <733kajaldas@gmail.com>
2026-02-13 13:36:27 +0530

    ok

--- app/Http/Controllers/WebsiteContoller.php
+++ app/Http/Controllers/WebsiteContoller.php
@@ -383,7 +383,7 @@ class WebsiteContoller extends Controller
         ->limit(4)
         ->get();
 
-        $result['collections'] = DB::table('collections')->where(['is_deleted'=>0])->orderBy('id','DESC')->limit(4)->get();
+        $result['collections'] = DB::table('collections')->where(['is_deleted'=>0])->orderBy('id','DESC')->limit(3)->get();
 
 
 
